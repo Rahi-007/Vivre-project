4767733e7a935b15298a7b5b839b103f9ec7f4b6
Rahi-007 <rubaiedahammed007@gmail.com>
2026-07-09 14:27:22 +0600

    animation added

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 //--------------{  Doctype.js  }--------------//
 
-
+AOS.init({
+    once: false,
+    duration: 1000,
+    easing: "ease-out",
+});
 
 ////------------{  Start Hare  }------------////
